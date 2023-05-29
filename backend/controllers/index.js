e4846a3901c710d1ce5c93ab3d041f94924f3cf3
Jitesh Kumar <jiteshece@gmail.com>
2023-05-29 14:33:36 +0530

    Create index.js in controller directroy

---
+++ backend/controllers/index.js
@@ -0,0 +1,5 @@
+export * from "auth.controller.js"
+export * from "user.controller.js"
+export * from "comment.controller.js"
+export * from "post.controller.js"
+export * from "password.controller.js"
