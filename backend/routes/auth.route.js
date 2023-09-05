36b1b513ede2b532f0762dd98c15f8f43ccc9a39
Jitesh Kumar <jiteshece@gmail.com>
2023-09-05 20:45:13 +0530

    router fixed

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,18 +1,15 @@
 const express = require("express");
 // const { register, login, logout } = require("../controllers/auth.controller")
 
-const authController = require("../controllers/auth.controller");
-
 const router = express.Router();
+const authController = require("../controllers/auth.controller");
 
 router.post("/signup", authController.register);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 
 // router.route("/register").post(register)
-
 // router.route("/login").post(login)
-
 // router.route("/logout").get(logout)
 
 module.exports = router;
--- backend/routes/password.route.js
+++ backend/routes/password.route.js
@@ -6,9 +6,7 @@ const express = require("express");
 // } = require("../controllers/password.controller")
 
 const passwordController = require("../controllers/password.controller");
-
 const { isAuthenticated } = require("../middlewares/auth.middleware");
-
 const router = express.Router();
 
 router.put(
@@ -25,9 +23,7 @@ router.put(
 router.put("/password/reset/:token", passwordController.resetPassword);
 
 // router.route("/update/password").put(isAuthenticated, updatePassword)
-
 // router.route("/forgot/password").post(forgotPassword)
-
 // router.route("/password/reset/:token").put(resetPassword)
 
 module.exports = router;
